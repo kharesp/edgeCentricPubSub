86722c7584961a9349b1787275a9fe6315f9255a
kharesp <shweta.p.khare@gmail.com>
2017-05-19 18:25:58 -0500

    zmq pubsub with flatbuffers

--- src/main/java/edu/vanderbilt/kharesp/clients/Publisher.java
+++ src/main/java/edu/vanderbilt/kharesp/clients/Publisher.java
@@ -4,12 +4,21 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
+import edu.vanderbilt.kharesp.types.DataSampleHelper;
+
 public class Publisher {
-	public static void main(String args[]){
+	public static void main(String args[]) throws InterruptedException{
 		Context context= ZMQ.context(1);
 		Socket publisher= context.socket(ZMQ.PUB);
-		
-		
-		
+		publisher.bind("tcp://*:5556");
+		Thread.sleep(1000);
+		for(int i=0;i<10;i++){
+			publisher.sendMore("alerts");
+			publisher.send(DataSampleHelper.serialize(i, 1, 1, 0, 11111, 10),0,0);
+			System.out.println("sent msg:"+i);
+			Thread.sleep(100);
+		}
+		publisher.close();
+		context.term();
 	}
 }
--- src/main/java/edu/vanderbilt/kharesp/clients/Subscriber.java
+++ src/main/java/edu/vanderbilt/kharesp/clients/Subscriber.java
@@ -1,5 +1,26 @@
 package edu.vanderbilt.kharesp.clients;
 
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZMsg;
+import edu.vanderbilt.kharesp.types.DataSampleHelper;
+
 public class Subscriber {
+	
+	public static void main(String args[]){
+		Context context= ZMQ.context(1);
+		Socket subscriber= context.socket(ZMQ.SUB);	
+		subscriber.connect("tcp://localhost:5556");
+		subscriber.subscribe("alerts".getBytes());
+
+		while(true){
+			ZMsg receivedMsg= ZMsg.recvMsg(subscriber);
+			System.out.println(new String(receivedMsg.getFirst().getData()));
+			System.out.println(DataSampleHelper.deserialize(receivedMsg.getLast().getData()).sampleId());
+			
+		}
+
+	}
 
 }
